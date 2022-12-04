33a66813ac8945c0a1f95006466cd458fc6eed79
Aleksandr <aleksanches123@mail.ru>
2022-12-04 05:50:30 +0300

    задача 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 1.");
+        byte age = 8;
+        if (age >= 18) {
+            System.out.println("Позравляем! Вы совершеннолетний!");
+        }
+        if (age < 18) {
+            System.out.println("Вы ещё не достигли совершеннолетия, подождите немного");
+        }
+    }
+}
