4f6bc6902b8f13d1efcc53dfd0011bb9f14b57c6
Aleksandr <aleksanches123@mail.ru>
2022-12-04 05:52:14 +0300

    задача 2

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,16 @@ public class Main {
         if (age < 18) {
             System.out.println("Вы ещё не достигли совершеннолетия, подождите немного");
         }
+        System.out.println("Задача 2.");
+        if (age >= 24) {
+            System.out.println("Человек окончил университет и ему пора искать первую работу");
+
+        }
+        if (age >= 18 && age < 24) {
+            System.out.println("Человек может отправляться в университет");
+        }
+        if (age >= 7 && age < 18) {
+            System.out.println("Ребёнок ходит в школу");
+        }
     }
 }
