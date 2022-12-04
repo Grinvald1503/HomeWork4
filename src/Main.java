de60eeff5599cd98102cc323a032189f09f13b1a
Aleksandr <aleksanches123@mail.ru>
2022-12-04 06:27:39 +0300

    задача 1.3

--- src/Main.java
+++ src/Main.java
@@ -35,5 +35,18 @@ public class Main {
         {
             System.out.println("Мест больше нет!");
         }
+        System.out.println("Задача 1.3");
+         if (age >= 2 && age <= 6) {
+             System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад");
+         }
+        else if (age >= 7 && age <= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу");
+        }
+         else if (age > 18 && age < 24) {
+             System.out.println("Если возраст человека равен " + age + ", то его место в университет");
+         }
+         else if (age > 24) {
+             System.out.println("Если возраст человека равен " + age + ", то ему пора ходить на работу");
+         }
     }
 }
